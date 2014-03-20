9838d95cc4120e491ceafb1bdf4a8c310172bb15
Hack Reactor Students <students@hackreactor.com>
2014-03-19 17:34:39 -0700

    successfully saved html stream to archive

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
-
+var httpRequest = require('http-request');
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
  * Consider using the `paths` object below to store frequently used file paths. This way,
  * if you move any files, you'll only need to change your code in one place! Feel free to
  * customize it in any way you wish.
  */
+var storage;
 
 exports.paths = {
   'siteAssets' : path.join(__dirname, '../web/public'),
@@ -26,16 +27,56 @@ exports.initialize = function(pathsObj){
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(){
+  fs.readFile(exports.paths.list, function(err, data){
+    console.log('inner function running, data is', data);
+    if (err) {
+      throw err;
+    }
+    storage = data.toString().split("\n");
+    // exports.isURLArchived('www.google.com');
+    // exports.isURLArchived('www.cnn.com');
+  });
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(url){
+// // assume input url is string
+// // return a boolean on whether string is in array
+  return storage.indexOf(url) !== -1;
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(url){
+  fs.appendFile(exports.paths.list, url+'\n', function (err) {
+    if (err) throw err;
+    console.log('url', url, ' appended!');
+  });
 };
 
-exports.isURLArchived = function(){
+exports.isURLArchived = function(url){
+  //return a boolean on whether or not there is a corresponding file inside the archive to the argument url;
+  fs.readdir(exports.paths.archivedSites, function(err, files){
+    if (err) throw err;
+    // console.log(files.indexOf(url) !== -1);
+    return files.indexOf(url) !== -1;
+  });
+
 };
 
-exports.downloadUrls = function(){
+exports.downloadUrls = function(url){
+  // url will be a string
+  //save all the html data of the passed in url , save it into a file inside archives/sites with
+  // name equal to url;
+  //invoked if isURLArchived is false
+  // var writeStream = fs.createWriteStream(exports.paths.archivedSites + "/" + url);
+  httpRequest.get({
+    url: url
+    // progress: function (current, total) {
+    //   console.log('downloaded %d bytes from %d', current, total);
+    // }
+  }, exports.paths.archivedSites + "/" + url, function (err, res) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(res.code, res.headers, res.file);
+  });
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,16 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+
 // require more modules/folders here!
+
+//set up the data storage for site.txt ;
+//
+
+archive.readListOfUrls();
+archive.downloadUrls('www.cnn.com');
 
 exports.handleRequest = function (req, res) {
   res.end(archive.paths.list);
 };
+
